cc2badab53f5683b43044e106435260627d9f929
Steven Shi <18601781218@163.com>
2020-12-24 10:16:38 +0800

    重构AbstractServerAdaptorConfig加载，把通讯协议改为通用的protocolMap

--- common/src/main/java/com/bizmda/bizsip/config/AbstractServerAdaptorConfig.java
+++ common/src/main/java/com/bizmda/bizsip/config/AbstractServerAdaptorConfig.java
@@ -8,20 +8,21 @@ import java.util.*;
  * @author shizhengye
  */
 @Data
-public abstract class AbstractServerAdaptorConfig {
+public class AbstractServerAdaptorConfig {
     private String id;
     private String type;
     private Map messageMap;
-    private AbstractServerAdaptorProtocolConfig protocol;
+    private Map protocolMap;
+//    private AbstractServerAdaptorProtocolConfig protocol;
 
     public AbstractServerAdaptorConfig(Map map) {
         this.id = (String)map.get("id");
         this.messageMap = (Map)map.get("message");
 
-        Map protocolMap = (Map)map.get("protocol");
-        String type = (String)protocolMap.get("type");
-        if (type.equalsIgnoreCase("java")) {
-            this.protocol = new JavaServerAdaptorProtocolConfig(protocolMap);
-        }
+        this.protocolMap = (Map)map.get("protocol");
+//        String type = (String)protocolMap.get("type");
+//        if (type.equalsIgnoreCase("java")) {
+//            this.protocol = new JavaServerAdaptorProtocolConfig(protocolMap);
+//        }
     }
 }
